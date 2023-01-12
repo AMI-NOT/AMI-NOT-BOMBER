dfce0f81fbfdc922b7141704cad936e0c46351b2
AMI-NOT <122160679+AMI-NOT@users.noreply.github.com>
2023-01-12 18:07:21 +0330

    Update AMI-NOT(sms).py

--- AMI-NOT(sms).py
+++ AMI-NOT(sms).py
@@ -4,6 +4,7 @@
 from colorama import init, Fore
 init()
 from random import choice, randint
+print("                                                          ")
 print(Fore.RED+'      》》》》》》》》 SMS BOMBER AMI-NOT《《《《《《《《')
 print("                                                                                                                      ")
 print(Fore.GREEN+'''
